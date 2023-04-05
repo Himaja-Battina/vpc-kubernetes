43223a9e1feec7818d3c4749737de6297bcfd9f4
Himaja-Battina <124758137+Himaja-Battina@users.noreply.github.com>
2023-04-05 16:17:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,6 +41,7 @@ resource "ibm_container_vpc_cluster" "cluster" {
   flavor            = "bx2.4x16"
   worker_count      = 1
   kube_version="1.25.8"
+  resource_group_id="IaC-dev"
  
   zones {
     subnet_id = ibm_is_subnet.subnet1.id
